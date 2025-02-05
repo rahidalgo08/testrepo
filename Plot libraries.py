0ebdd37f40fa26d685101f4817da7584ca750b57
rahidalgo08 <rodrigo.hidalgo.rhm@gmail.com>
2025-02-05 16:26:03 -0500

    Add files via upload

---
+++ Plot libraries.py
@@ -0,0 +1,91 @@
+#### Popular plot libraries ####
+# La visualización de datos es una herramienta vital para obtener información y comunicarse de
+# manera eficaz con información compleja. Hay diversas librerías en python que proporcionan 
+# funciones únicas y capacidades. Algunas de las más populares bibliotecas son: Matplotlib, 
+# Pandas, Seaborn, Plotly, PyWaffle. Cada biblioteca tiene sus puntos fuertes y casos de uso propio.
+# Aprovechando el poder de estas bibliotecas de gráficos, puede obtener información de sus datos
+# y comunicas hallazgos de manera eficaz.
+
+
+### i) matplotlib: Es una biblioteca de trazado de uso general que proporciona una amplia
+#                 variedad de gráficos y opciones de personalización. Se integra bien con otras
+#                 bibliotecas y marcos como NumPy, Pandas, Seaborn y Plotly. Esta 
+#                 interoperabilidad  le permite combinar puntos fuertes de las diferentes 
+#                 bibliotecas y crear bibliotecas sofisticadas y visualizaciones interactivas.
+
+## Características de matplotlib ##
+# a) Puedes crear plot lines, scatter plots, bar plots, histogramas, gráficos circulares, 
+# box plots y heat mapz, por nombras algunos. 
+# b) Te permite personalizar colores, estilos de línea, estilos de marcadores, etiquetas de
+# acesso, títulos, leyendas, etc, la versatilidad de matplotlib, opciones de personalización,
+# capacidades de integración y el apoyo de la comunidad lo covierte en una opción poderosa y 
+# confiable para tareas de visualización de datos en python.
+
+
+
+### ii) Pandas:   Los usuarios lo emplean principalmente para la manipulación de datos, pero
+#                 también ofrece capacidades de trazado. Funciones de trazado de Pandas están
+#                 construidas sobre matplotlib, lo que significa que puedes aprovechar la
+#                 potencia y versatilidad de Matplotlib al combinarlo con los datos de Pandas y 
+#                 sus capacidades de manipulación. 
+
+## Características de Pandas ##
+# a) Se integra a la perfeccción con las estructuras de datos de Pandas, lo que facilita la 
+# creación de lines plots, scatter plots, bar plots, histogramas y gráficos circulares 
+# directamente desde sus dataframe.
+# b) Las capacidades de visualización de los pandas están bien adecuado para el análisis 
+# exploratorio de datos. 
+
+
+
+### iii) Seaborn: Al igual que pandas, Seaborn está también construido sobre la funcionalidad 
+#                 de Matplotlib. Es una gran opción para visualizaciones estadísticas 
+#                 especializadas. Proporciona opciones elegantes y especializadas tipos de
+#                 gráficos tales como gráficos categóricos, gráficos de conteo, heat maps,
+#                 gráficos de violín, scatter plots, bar plots y muchos más.
+
+## Características de Seaborn ##
+# a) Viene con varias paletas de colores y estilos para personalizar y mejorar la estética del
+# gráfico. 
+# b) Ofrece funciones para combinar varios gráficos en diseños de cuadrícula, lo que puede ser
+# útil para comparar múltiples variables o subgrupos.
+# c) Su integración con Pandas permite trazar datos directamente desde los marcos o series de
+# datos de Pandas. La combinación de Seaborn de estadísticas de visualizaciones por defecto,
+# la integración estética con la facilidad de uso de Panda y los temas incorporados lo
+# convierten en una popular elección entre científicos y analistas de datos para explorar y
+# comunicar la información a partir de los datos.
+
+
+
+### iv) Folium:   Cuando se trata de lo geoespacial para la visualización de datos, Folium es
+#                 una excelente biblioteca. Te permite crear contenido interactivo y mapas
+#                 personalizables. Ya sean mapas de coroplasmas, mapas de puntos o heat maps,
+#                 Folium proporciona las herramientas para visualizar representaciones de los
+#                 datos geoespaciales.
+
+## Características de Folium ##
+# a) Se integra a la perfección con bibliotecas de análisis de datos en Python, como Pandas y
+# NumPy. Es una opción popular para visualización de datos geoespaciales y análisis en Python.
+
+
+
+### v) Plotly:    Si quieres presentar tus datos de forma interactiva, plotly es una excelente
+#                 opción para explorar. Ofrece gráficos altamente interactivos y tableros de
+#                 mandos(dashboards). Con plotly, puede crear gráficos de líneas, scatter plots,
+#                 gráficos de barras, gráficos circulares, gráficos 3D y mapas de coropletas.
+
+## Características de Plotly ##
+# a) Su entorno de trabajo permite crear paneles de control interactivos con visualizaciones
+# enriquecidas y controlables. Como plotly está basado en la web, permite la representación y
+# visualización de gráficos en navegadores web. Esto hace que sea conveniente para compartir
+# visualizaciones en línea, incrustarlas en aplicaciones web o paneles de control y 
+# colaboración con otros. 
+
+
+
+### vi) PyWaffle:  Si quieres visualizar datos categóricos usando gráfico de waffles, PyWaffle
+#                  es simple, pero efectivo.
+
+## Características de PyWaffle ##
+# a) Puedes crear gráficos waffles, gráficos circulares, gráficos de anillos y muchos más tipos
+# de gráficos al proporcionar una forma única de representar proporciones.  
